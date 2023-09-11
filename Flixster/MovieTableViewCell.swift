31e4f917a3987dca197728386815fe86ac796b22
Patrick <bakerpatrick644@yahoo.com>
2023-09-11 14:29:28 -0400

    commit 1

---
+++ Flixster/MovieTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewCell.swift
+//  Flixster
+//
+//  Created by Patrick Baker on 9/11/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
